0c1597a232fe8c0d016eb1a33ddf89f1cbfbbafd
pinglu85 <luping8576@hotmail.com>
2022-07-23 19:34:01 +0200

    feat: remove browser default styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,7 +38,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Merriweather&display=swap"
           rel="stylesheet"
         />
       </Head>
