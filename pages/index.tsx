f71b8f52a4cc737232cd7145880a3a3ae69d99df
pinglu85 <luping8576@hotmail.com>
2022-07-23 20:49:17 +0200

    update homepage content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,90 +1,40 @@
 import Head from 'next/head';
-import Image from 'next/image';
+
+import ProfileImage from './profile-image.svg';
+import DribbbleIcon from './dribbble-brands.svg';
+import GithubIcon from './github-brands.svg';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Ping Lu</title>
         <meta name="description" content="Frontend Developer" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0e0e0e" />
-        <meta name="msapplication-TileColor" content="#0e0e0e" />
-        <meta name="theme-color" content="#ffffff" />
       </Head>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <div className={styles.profileImage}>
+            <ProfileImage />
+          </div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <h1 className={styles.name}>Ping Lu</h1>
+          <h4 className={styles.description}>Frontend Developer</h4>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <p className={styles.text}>
+            The website is currently under construction.
+          </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+          <div className={styles.socialMediaContainer}>
+            <a href="https://github.com/pinglu85">
+              <GithubIcon />
+            </a>
+            <a href="https://dribbble.com/pinglu">
+              <DribbbleIcon />
+            </a>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
