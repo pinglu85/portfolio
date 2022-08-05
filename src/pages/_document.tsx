a9fd37b7c83a68013214036d97918fce346f4fe4
pinglu85 <luping8576@hotmail.com>
2022-08-05 23:22:36 +0200

    change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,28 +7,7 @@ const MyDocument = () => {
         <meta charSet="UTF-8" />
 
         {/* favicon */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0e0e0e" />
-        <meta name="msapplication-TileColor" content="#0e0e0e" />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
 
         {/* fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
